a31e9cfe6cb0e8546509b6a787471156da0d9fd4
IzzuddinAzzaky <113503305+IzzuddinAzzaky@users.noreply.github.com>
2023-01-04 23:11:52 +0700

    Add files via upload

---
+++ src/test/java/com/izzuddinazzaky/pengembalianservice/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izzuddinazzaky.pengembalianservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
